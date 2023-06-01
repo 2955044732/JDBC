a133812563b5cd7833dbfc77cf77b31b3a169dbf
qwh <qiuwanhong02@163.com>
2023-06-01 10:54:44 +0800

    init commit

--- src/main/java/com/qwh/jdbc.java
+++ src/main/java/com/qwh/jdbc.java
@@ -4,11 +4,10 @@ package com.qwh;
 import com.mysql.cj.jdbc.Driver;
 import org.junit.Test;
 
-import java.io.IOException;
+
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 
