c5a932e67fde978e1b400f339e783cb349d603b3
qwh <qiuwanhong02@163.com>
2023-06-01 11:29:48 +0800

    User commit

---
+++ src/main/java/com/qwh/User.java
@@ -0,0 +1,20 @@
+package com.qwh;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @auther qwh
+ * @create 2023-06-2023/6/1 11:17
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    private String user;
+    private String password;
+
+}
