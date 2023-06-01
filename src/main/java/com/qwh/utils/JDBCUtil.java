7591dfa91055efad5467fd637d2484aef2d40ec1
qwh <qiuwanhong02@163.com>
2023-06-01 19:49:45 +0800

    commit

--- src/main/java/com/qwh/utils/JDBCUtil.java
+++ src/main/java/com/qwh/utils/JDBCUtil.java
@@ -4,6 +4,8 @@ package com.qwh.utils;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -79,7 +81,7 @@ public class JDBCUtil {
                     Object columnValue = rs.getObject(i + 1);
                     String columnLabel = rsmd.getColumnLabel(i + 1);
 
-                    Field field = t.getClass().getDeclaredField(columnLabel);
+                    Field field =clazz.getDeclaredField(columnLabel);
                     field.setAccessible(true);
                     field.set(t,columnValue);
                 }
@@ -93,6 +95,52 @@ public class JDBCUtil {
         return null;
     }
 
+    public static  <T> List<T> queryForList(Class<T> clazz,String sql, Object... args){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtil.getConnect();
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            // 获取结果集的元数据 :ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            // 通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+            //创建集合对象
+            ArrayList<T> list = new ArrayList<T>();
+            while (rs.next()) {
+                T t = clazz.newInstance();
+                // 处理结果集一行数据中的每一个列:给t对象指定的属性赋值
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取列值
+                    Object columValue = rs.getObject(i + 1);
+
+                    // 获取每个列的列名
+                    // String columnName = rsmd.getColumnName(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    // 给t对象指定的columnName属性，赋值为columValue：通过反射
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columValue);
+                }
+                list.add(t);
+            }
+
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.closeSource(conn, ps, rs);
+        }
+        return null;
+    }
 
     /**
      * 关闭资源
