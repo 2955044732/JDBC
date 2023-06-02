03d1f163e061784c1c9a02db93beadb712bbc825
qwhong <qiuwanhong02@gmail.com>
2023-06-02 18:41:21 +0800

    h

---
+++ src/main/java/com/qwh/bean/Goods.java
@@ -0,0 +1,19 @@
+package com.qwh.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @Auther: QWH
+ * @Date: 2023-06-02-18:25
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Goods {
+    private Integer id;
+    private String name;
+}
